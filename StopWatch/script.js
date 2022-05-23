ea4426f279c833bdd8db68d8b26a5d37fdc7ebcb
Sasen Perera <sasen.learnings@gmail.com>
2022-05-23 17:18:06 +0530

    StopWatch-Done

---
+++ StopWatch/script.js
@@ -0,0 +1,31 @@
+let time = 0.00;
+let stop = false;
+
+function sleep(ms){
+    return new Promise(resolve => setTimeout(resolve, ms));
+};
+
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+$(function(){
+  $("#start").click(async function(){
+    stop = false
+    while (stop === false) {
+        time += 0.01
+        time = round(time, 2)
+        console.log(time)
+        $("#clock").text(time);
+        await sleep(0.5)
+    }
+  });
+  $("#stop").click(async function(){
+      stop = true
+      $("#clock").text(time)
+  });
+  $("#restart").click(async function(){
+      time = 0.00
+      $("#clock").text(time)
+  })
+});
