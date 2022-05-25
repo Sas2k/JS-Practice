0cc96ff02e577edf64d461e456d7c4d981df7a82
Sasen Perera <sasen.learnings@gmail.com>
2022-05-25 18:56:34 +0530

    Nothing Much

--- ColorGuessingGame/Script.js
+++ ColorGuessingGame/Script.js
@@ -9,16 +9,17 @@ const generateColors = () => {
     delete color_ops[randomElement];
     let color_th = color_ops;
     for (let index = 0; index < color_th.length; index++) {
-        const element = color_th[index];
-        element.css("background-color", `rgb(${randomBetween(0, 255)},${randomBetween(0, 255)},${randomBetween(0, 255)})`);   
+        let element = color_th[index];
+        let value = `rgb(${randomBetween(0, 255)},${randomBetween(0, 255)},${randomBetween(0, 255)})`
+        element.css("background-color", value);   
     }
-    answer.css("background-color", value);
+    randomElement.css("background-color", value);
     return ""
 };
 
-$(function(){
+$(document).ready(function(){
     $("#new").click(function(){ 
         generateColors();
-    });
+    })
     generateColors();
 });
